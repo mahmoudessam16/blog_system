2e3a1df619cf73cc4b4d4fe72c1bf8aa0599d109
mahmoudessam16 <mahmoudemourad29@gmail.com>
2025-06-15 23:05:26 +0300

    fix base_url

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: `${import.meta.env.VITE_REACT_APP_BACKEND_URL}`,
+  baseURL: import.meta.env.VITE_REACT_APP_BACKEND_URL,
 });
 
 // Attach token if found
