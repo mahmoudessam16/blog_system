1f8e8abb53fa1f6b0f0cc764c7d61c28bce61a8b
mahmoudessam16 <mahmoudemourad29@gmail.com>
2025-06-15 23:17:43 +0300

    fix origin in cors

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "https://blog-system-front.vercel.app",
+    origin: "https://blog-system-front2.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
